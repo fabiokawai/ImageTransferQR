d07bf7944d83c4a2bbb0db8cf396f197dfb4f31a
Fábio Kawai <fabiokawai@outlook.com>
2018-09-30 04:16:56 -0300

    Implemented QR Scan

--- app/src/main/java/com/example/fabiokawai/imagetransferqr/DownloadActivity.java
+++ app/src/main/java/com/example/fabiokawai/imagetransferqr/DownloadActivity.java
@@ -1,25 +1,58 @@
 package com.example.fabiokawai.imagetransferqr;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class DownloadActivity extends AppCompatActivity {
 
+    TextView txtResultCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
 
+        txtResultCode = (TextView) findViewById(R.id.txtResultCode);
+
+        IntentIntegrator integrator = new IntentIntegrator(DownloadActivity.this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
 
     }
 
     public void btnSave(View view) {
         //SALVAR IMAGEM NA GALERIA
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.e("Scan*******", "Cancelled scan");
+
+            } else {
+                Log.e("Scan", "Scanned");
+                txtResultCode.setText(result.getContents().toString());
+//                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 
